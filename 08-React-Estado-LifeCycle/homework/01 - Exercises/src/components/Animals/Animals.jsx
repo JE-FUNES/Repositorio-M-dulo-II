7f14a8a1680fb247d1a7229b59dc6da905abd3ed
JE-FUNES <infoventacba@gmail.com>
2023-08-23 08:59:45 -0300

    Ejercicios Estados Test ok

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -3,10 +3,32 @@ import React from 'react';
 
 export default class Animals extends React.Component {
   constructor(props) {
-    super();
+    super(props);
   }
 
+  /* Dentro del div, mapea y renderiza las props animals de acuerdo a lo que necesitemos:
+Por cada animal, debemos renderizar un div con lo siguiente:
+Una etiqueta h5 con el nombre del animal.
+Una etiqueta img con los atributos:
+src asignando como valor la imagen del animal.
+alt asignando como valor el nombre del animal.
+width con un valor de 300px (para darle un tamaño apropiado a la imagen).
+Una etiqueta span con la especie del animal.*/
+
+
   render() {
-    return <div></div>
-  }
+    return (
+    <div>
+      {this.props.animals.map((animal, index) => {
+        return (
+        <div key={index}>
+          <h5>{animal.name}</h5>
+          <img src={animal.image} alt={animal.name} width="300px" />
+          <span>{animal.specie}</span> 
+        </div>
+  );
+        })}
+    </div>
+  );
+}
 }
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -7,11 +7,73 @@ import Species from "../Species/Species";
 
 export default function Zoo() {
   /* Escribe acá tu código */
+  let [zoo, setZoo] = React.useState({
+    zooName: "",
+    animals: [],
+    species: [],
+    allAnimals: [],
+  });
 
+  /*Crea una función llamada handleInputChange, que reciba un evento como parámetro.
+    Dentro de la función handleInputChange, setea del estado zoo, la propiedad zooName, 
+    capturando el valor del input. */
+  function handleInputChange(evento) {
+    setZoo({ ...zoo, zooName: evento.target.value });
+  } 
+
+  /*Utiliza el hook React.useEffect.
+Hint: No olvides que el hook ReactuseEffect recibe dos parámetros. 💡
+
+Nota: Para que corran los test, el hook debe ser utilizado de esta manera: React.useEffect(). 
+No debe utilizarse como useEffect(). 💡
+
+Dentro del hook, usa fetch para hacer una petición al servidor db.json a través de la url 
+'http://localhost:3001/zoo'. Así obtendrás el objeto zoo con los datos de los animales. 
+Para utilizar fetch, es necesario usar promesas. 
+Como aún no las has visto, tienes este snippet para que copies y pegues dentro del hook React.useEffect:*/
+  React.useEffect(() => {
+    fetch('http://localhost:3001/zoo')
+   .then((res) => res.json())
+   .then((data) =>
+      setZoo({
+         ...zoo,
+         animals: data.animals,
+         species: data.species,
+         allAnimals: data.animals,
+      })
+   )
+   .catch((error) => console.log(error));
+  }, []);
+
+  /*Crea una función llamada handleSpecies, que reciba un evento como parámetro.
+Crea una función llamada handleAllSpecies.*/
+
+  const handleSpecies = (event) => {
+    const specie = event.target.value;
+
+    setZoo({
+      ...zoo,
+      animals: zoo.allAnimals.filter((animal) => animal.specie === specie),
+    });
+  }
+
+  const handleAllSpecies = () => {
+    setZoo({
+      ...zoo, 
+      animals: zoo.allAnimals 
+    })
+  };
+    
 
   return (
     <div>
-      <h1>-</h1>
+      
+      <label>Zoo Name:</label>
+      <input id="zooName" onChange={handleInputChange} value={zoo.zooName}></input>
+      <h1>{zoo.zooName}</h1>
+      <Species species={zoo.species} handleSpecies={handleSpecies} handleAllSpecies={handleAllSpecies} />
+      <Animals animals={zoo.animals} />
+      
     </div>
   );
 }
